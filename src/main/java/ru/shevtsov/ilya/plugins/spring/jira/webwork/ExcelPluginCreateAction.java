bd5f219fd7ddcdb4f79d10f9d40ffd46f9888459
Шевцов Илья Александрович <ishevtsov@dellin.local>
2023-06-26 18:54:29 +0300

    got projectId

--- src/main/java/ru/shevtsov/ilya/plugins/spring/jira/webwork/ExcelPluginCreateAction.java
+++ src/main/java/ru/shevtsov/ilya/plugins/spring/jira/webwork/ExcelPluginCreateAction.java
@@ -14,21 +14,30 @@ public class ExcelPluginCreateAction extends JiraWebActionSupport {
     ExcelExportService excelExportService;
     List<Issue> relIssues;
     String epic = "BPM. Развитие и оптимизация 3PL 2023";
-
+    private String projectKey;
     @Inject
     public ExcelPluginCreateAction(ExcelExportService excelExportService) {
         this.excelExportService = excelExportService;
     }
 
+    public String getProjectKey() {
+        return projectKey;
+    }
+
+    public void setProjectKey(String projectKey) {
+        this.projectKey = projectKey;
+    }
+
+
     @Override
-    public String execute() throws Exception {
+    public String doDefault() throws Exception {
         try {
             System.out.println(excelExportService);
             relIssues = excelExportService.getRelevantIssue();
         } catch (Exception e) {
             System.out.println("EXCEPTIONS!!!!!!!!!!>>>>>>>>>>" + e);
         }
-
+        System.out.println("EXCEPTIONS!!!!!!!!!!>>>>>>>>>>" + projectKey);
         Map<Integer, Object[]> issueMap = new TreeMap<Integer, Object[]>();
 
         for (Issue issue : relIssues)
@@ -50,11 +59,48 @@ public class ExcelPluginCreateAction extends JiraWebActionSupport {
 //        IWriteExcel iWriteExcel = null;
 //        iWriteExcel.WriteExcel(issueMap, epic);
 
-        System.out.println("\n\n" + issueMap + "\n\n");
-        System.out.println("\n\n" + issueMap.get(10005)[0] + "\n\n");
+        //System.out.println("\n\n" + issueMap + "\n\n");
+        //System.out.println("\n\n" + issueMap.get(10005)[0] + "\n\n");
 
         return SUCCESS;
+
     }
+
+//    @Override
+//    public String execute() throws Exception {
+//        try {
+//            System.out.println(excelExportService);
+//            relIssues = excelExportService.getRelevantIssue();
+//        } catch (Exception e) {
+//            System.out.println("EXCEPTIONS!!!!!!!!!!>>>>>>>>>>" + e);
+//        }
+//        System.out.println("EXCEPTIONS!!!!!!!!!!>>>>>>>>>>" + projectKey);
+//        Map<Integer, Object[]> issueMap = new TreeMap<Integer, Object[]>();
+//
+//        for (Issue issue : relIssues)
+//        {
+//            //data.put(0, new Object[] {"BPM3PL3-1", "Тема", "Продукт/инструмент для работы с большими/сложными клиентами", "", "", "Открыто"});
+//
+//            issueMap.put(issue.getId().intValue(), new Object[] { issue.getKey(), issue.getIssueType().getName(),
+//                    issue.getSummary(), issue.getReporter(), issue.getDueDate(), issue.getAssignee(),
+//                    issue.getStatus().getName() });
+//
+//            for (Issue subTask : issue.getSubTaskObjects())
+//            {
+//                issueMap.put(subTask.getId().intValue(), new Object[] { subTask.getKey(), subTask.getIssueType().getName(),
+//                        subTask.getSummary(), subTask.getReporter(), subTask.getDueDate(), subTask.getAssignee(),
+//                        subTask.getStatus().getName() });
+//            }
+//        }
+//
+////        IWriteExcel iWriteExcel = null;
+////        iWriteExcel.WriteExcel(issueMap, epic);
+//
+//        //System.out.println("\n\n" + issueMap + "\n\n");
+//        //System.out.println("\n\n" + issueMap.get(10005)[0] + "\n\n");
+//
+//        return SUCCESS;
+//    }
 
     public Byte[] getExcelFile() {
         //relIssues.forEach(issue -> );
